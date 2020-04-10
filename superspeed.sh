5a40fe9862c8928348284f5ce2712adea24f8853
root <29743004+zq@users.noreply.github.com>
2020-04-10 14:13:59 +0800

    Update superspeed.sh

--- superspeed.sh
+++ superspeed.sh
@@ -114,18 +114,8 @@ preinfo() {
 	echo "——————————————————————————————————————————————————————————"
 }
 
-selecttest() {
-	echo -e "  测速类型:    ${GREEN}1.${PLAIN} 三网测速    ${GREEN}2.${PLAIN} 取消测速"
-	echo -ne "               ${GREEN}3.${PLAIN} 电信节点    ${GREEN}4.${PLAIN} 联通节点    ${GREEN}5.${PLAIN} 移动节点"
-	while :; do echo
-			read -p "  请输入数字选择测速类型: " selection
-			if [[ ! $selection =~ ^[1-5]$ ]]; then
-					echo -ne "  ${RED}输入错误${PLAIN}, 请输入正确的数字!"
-			else
-					break   
-			fi
-	done
-}
+
+$selection=1
 
 runtest() {
 	[[ ${selection} == 2 ]] && exit 1
